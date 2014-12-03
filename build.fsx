7c7e416761391a69628e4cb1052498df28fe9bb3
Loic Denuziere <loic.denuziere@intellifactory.com>
2014-12-03 18:40:20 +0100

    Version 3.0-alpha; Apache license

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("WebSharper.Google.Visualization", "2.5")
+    BuildTool().PackageId("WebSharper.Google.Visualization", "3.0-alpha")
         .References(fun r -> [r.Assembly "System.Web"])
     |> fun bt -> bt.WithFramework(bt.Framework.Net40)
 
